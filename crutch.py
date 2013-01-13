9ad3c74c126b4b02d9401f2d26f07c8e58bf45d4
Jim Kalafut <jim@kalafut.net>
2013-01-12 22:28:38 -0800

    Adding ability to traverse nested folders

--- crutch.py
+++ crutch.py
@@ -52,7 +52,7 @@ def replace_tag(match):
     fn = match.groups()[0]
     with open(join(ASSET_DIR, fn), 'r') as f:
         contents = f.read()
-    return "<script>{contents}</script>".format(contents=contents)    
+    return "<script>{contents}</script>".format(contents=contents)
 
 def parse_template(name, data):
     with open(join(TEMPLATE_DIR, name), "r") as f:
@@ -166,6 +166,87 @@ def sections_matches(project, section, config):
 def validate_section(section):
     pass
 
+def lp2(path, project_path = [], db = {"projects":[]}):
+    """
+    Traverse the projects tree, identify and loading matching projects.
+    """
+
+    # Check current folder
+    prj_desc = load_project_desc(join(path,"_project.yml"))
+    if prj_desc:
+        new_project = { "project":prj_desc["project"], "sections": [], "uid": next_uid() }
+        db["projects"].append(new_project)
+
+        project_path.append(prj_desc["project"])
+        for section_file in [ join(path,name) for name in os.listdir(path) if os.path.splitext(name)[1] == ".yml" and not name == "_project.yml" ]:
+            new_project["sections"] += load_sections(section_file)
+
+    for name in os.listdir(path):
+        next_path = join(path,name)
+        if os.path.isdir(next_path):
+            lp2(next_path, project_path, db)
+
+    if prj_desc:
+        project_path.pop()
+
+    return db
+
+def load_project_desc(filename):
+    content = None
+
+    if os.path.exists(filename):
+        with open(filename, "r") as f:
+            content = yaml.load(f)
+
+    return content
+
+def load_sections(filename):
+    """
+    Load a section .yml file. This will usually result in one section structure,
+    but YAML will permit multiple document in file. Therefore an array of documents
+    will always be returned.
+    """
+    sections = []
+    print("Loading " + filename)
+    with open(filename, "r") as f:
+        for content in yaml.load_all(f):
+            content['uid'] = next_uid()
+
+            """
+            if "section" not in content:
+                exit("%s: missing 'section:' declaration" % infile)
+
+            if content["section"] in prj:
+                exit("Error: duplicate section: %s" % infile)
+"""
+
+            if True: #sections_matches(prj["project"], content, config):
+                # entries
+                section_url = content.get("url")
+
+                entry_array = []
+                for entry in content['entries']:
+                    if len(entry) > 3 or (len(entry) == 3 and not type(entry[2]) == dict):
+                        exit("Invalid entry declaration in %s/%s: " % (project, content["section"])  + repr(entry))
+
+                    #TODO this is not correctly handling just a description plus options
+                    if len(entry) == 1:
+                        new_entry = { "description": entry[0], "uid": next_uid() }
+                    elif len(entry) >= 2:
+                        new_entry = { "term": entry[0], "description": entry[1], "uid": next_uid() }
+
+                    if len(entry) > ENTRY_OPT:
+                        new_entry["options"] = entry[ENTRY_OPT]
+                    else:
+                        new_entry["options"] = {}
+                    new_entry['entry_url'] = prepare_entry_url(new_entry, section_url)
+                    entry_array.append(new_entry)
+
+                content["entries"] = entry_array
+                sections.append(content)
+    return sections
+
+
 def load_projects(path, config):
     db= { 'projects': [] }
 
@@ -269,12 +350,12 @@ def random_project():
 def random_section(project, entries):
     s = random_word(5,10)
     with open(join(PROJECTS_DIR, project, s+".yml"), "w") as f:
-        f.write("""section: %s 
+        f.write("""section: %s
 url: http://example.cm/{entry}
 sort: fixed
 entries:\n""" % (s))
         for x in range(entries):
-            f.write('    - [ "%s", "%s" ]\n' % (random_word(4,10), random_sentence(5,20))) 
+            f.write('    - [ "%s", "%s" ]\n' % (random_word(4,10), random_sentence(5,20)))
 
 def stress_test(projects, sections, entries):
     for i in range(projects):
@@ -306,7 +387,8 @@ def main():
 
     config = load_config(args.config)
 
-    db = load_projects("projects", config)
+    #db = load_projects("projects", config)
+    db = lp2("projects")
     html = parse_template(config['template'] + ".html", db)
 
     with open("index.html", "w") as out:
